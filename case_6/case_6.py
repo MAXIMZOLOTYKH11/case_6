e0cca1a994dfac396dbca7fc4ae631fa69d77bb9
MAXIMZOLOTYKH11 <max.zl@mail.ru>
2018-12-01 15:44:04 +0700

    initial commit

---
+++ case_6/case_6.py
@@ -0,0 +1,93 @@
+from turtle import *
+from math import *
+width(1)
+
+
+def d(n):
+    p = 500 / int(n)
+    return p
+
+
+def h(p):
+    d1 = p / 2
+    s = d1 / (sqrt(3) / 2)
+    return s
+
+
+def figure(s, p, n):
+    for o in range(2):
+        color('black')
+        yell = 'yellow'
+        gr = 'green'
+        nn = 1
+        n = int(n)
+        for i in range(n):
+            if nn % 2 == 0:
+                fillcolor(gr)
+            else:
+                fillcolor(yell)
+            begin_fill()
+            right(-30)
+            forward(s)
+            for q in range(5):
+                right(60)
+                forward(s)
+            end_fill()
+            right(90)
+            penup()
+            forward(p)
+            pendown()
+            nn += 1
+            continue
+        right(90)
+        penup()
+        forward(s)
+        forward(s)
+        left(-60)
+        forward(s)
+        pendown()
+        left(-30)
+        for i in range(n):
+            if nn % 2 == 0:
+                fillcolor(gr)
+            else:
+                fillcolor(yell)
+            begin_fill()
+            right(-30)
+            forward(s)
+            for q in range(5):
+                right(60)
+                forward(s)
+            end_fill()
+            right(90)
+            penup()
+            forward(p)
+            pendown()
+            nn += 1
+            continue
+        right(90)
+        penup()
+        forward(s)
+        forward(s)
+        left(-60)
+        forward(s)
+        left(-30)
+        right(90)
+        forward(3 * s)
+        right(-90)
+        pendown()
+        mainloop()
+
+
+
+def main():
+    n = numinput('n', 'Пожалуйста, введите количество шестиугольников, располагаемых в ряд:', 0, minval=4, maxval=20)
+    p = d(n)
+    a = h(p)
+    figure(a, p, n)
+    figure(a, p, n)
+
+
+
+if __name__ == '__main__':
+    main()
